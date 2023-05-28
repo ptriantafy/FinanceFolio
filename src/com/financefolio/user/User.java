7d45d893bfa086bc717c40b9f8ae3ac5f413d56d
BillPapadopoulos <billaros218@gmail.com>
2023-05-28 14:58:02 +0300

    user change

--- src/com/financefolio/user/User.java
+++ src/com/financefolio/user/User.java
@@ -1,14 +1,40 @@
 package com.financefolio.user;
 
+import java.util.Scanner;
+import com.financefolio.user.User;
+import java.util.Scanner;
+
 public class User {
 	private int id;
 	private String name;
 	
+   public static void main(String[] args) {
+	
+	 Scanner scanner = new Scanner(System.in);
+	 System.out.println("Enter user ID:");
+	 int userId = scanner.nextInt();
+	 scanner.nextLine();
+	 System.out.println("Enter user name:");
+	 String userName = scanner.nextLine();
+	
+	 User newUser = new User(userId, userName);
+	 newUser.displayUserInfo();
+	 
+	 Member member = new Member(newUser.getId(), newUser.getName(), false, 0, 0.0f, 0, 0);
+	 member.Choice();
+	 }
+	 
 	public User(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+	
+	protected void displayUserInfo() {
+	    System.out.println("User ID: " + id);
+	    System.out.println("User Name: " + name);
+	}
+	
 	public int getId() {
 		return id;
 	}
